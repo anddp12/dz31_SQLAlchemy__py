7f0746a5100a4d8f2e2ef881408938e2ff50f026
anddp12 <anddp12@gmail.com>
2023-02-09 00:29:15 +0200

    creating a Class Teachers and Academy_db

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from sqlalchemy.orm import DeclarativeBase
 from sqlalchemy.orm import Mapped
 from sqlalchemy.orm import mapped_column
+from sqlalchemy import create_engine
 
 
 class Base(DeclarativeBase):
@@ -39,3 +40,25 @@ class Groups(Base):
 
     def __repr__(self) -> str:
         return f"Groups#(id={self.id!r}, name={self.name!r}, rating={self.rating!r}, rating={self.year!r})"
+
+
+class Teachers(Base):
+    __tablename__ = "Teachers"
+
+    id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
+    first_name: Mapped[str] = mapped_column()
+    last_name: Mapped[str] = mapped_column()
+    position: Mapped[str] = mapped_column()
+    employmentDate: Mapped[str] = mapped_column()
+    salary: Mapped[float] = mapped_column()
+    premium: Mapped[float] = mapped_column(default=0)
+    isAssistant: Mapped[int] = mapped_column(default=0)
+    isProfessor: Mapped[int] = mapped_column(default=0)
+
+    def __repr__(self) -> str:
+        return f"Teachers#(id={self.id!r}, first_name={self.first_name!r}, last_name={self.last_name!r}, position={self.position!r}, employmentDate={self.employmentDate!r}, salary={self.salary!r}, premium={self.premium!r})"
+
+engine = create_engine("sqlite:///Academy_db.db")
+
+Base.metadata.drop_all(engine)
+Base.metadata.create_all(engine)
