fd27f92ac6534f285b8b128bacf8c93ebc129dc8
anddp12 <anddp12@gmail.com>
2023-02-09 00:18:30 +0200

    creating a class Departments

---
+++ main.py
@@ -0,0 +1,18 @@
+from sqlalchemy.orm import DeclarativeBase
+from sqlalchemy.orm import Mapped
+from sqlalchemy.orm import mapped_column
+
+
+class Base(DeclarativeBase):
+    pass
+
+
+class Departments(Base):
+    __tablename__ = "Departments"
+
+    id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
+    name: Mapped[str] = mapped_column(unique=True)
+    financing: Mapped[float] = mapped_column(default=0)
+
+    def __repr__(self) -> str:
+        return f"Departments(id={self.id!r}, name={self.name!r}, financing={self.financing!r})"
